9048ad5245823ace16141d460d831185c3c3e775
Nikita <142217578+JOkoropuzz@users.noreply.github.com>
2023-11-16 22:11:38 +0300

    Add files via upload

---
+++ Lab01/triangleSquare/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Reflection.Metadata;
+
+double pirimeter;
+double triangleSide;
+double square;
+Console.WriteLine("Enter the perimeter of the equilateral triangle");
+pirimeter = double.Parse(Console.ReadLine());
+triangleSide = pirimeter / 3;
+square = Math.Sqrt((pirimeter / 2) * Math.Pow(((pirimeter / 2) - triangleSide), 3));
+Console.WriteLine("Side | Square");
+Console.WriteLine(Math.Round(triangleSide, 2) + " | " + Math.Round(square, 2));
