e8109435cc4159610f85a4700d695d62fac5b817
Nikita <142217578+JOkoropuzz@users.noreply.github.com>
2023-11-21 20:04:30 +0300

    Add files via upload

---
+++ Alg/AlgTask/Sqrrt.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgTask
+{
+    internal class Sqrrt
+    {
+        static double sqrtIt(double target)
+        {
+            double a = 1;
+            double olda;
+            do
+            {
+                
+                olda = a;
+                a = (a + target / a) / 2;
+            }
+            while (olda != a);
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            double target = 144;
+            Console.WriteLine(sqrtIt(target));
+            Console.WriteLine(sqrtIt(target) * sqrtIt(target));
+        }
+    }
+}
---
+++ Alg/Loop_demo/SumDigit.cs
@@ -0,0 +1,20 @@
+﻿namespace modu_2_
+{
+    class LoopProgram
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter integer number: ");
+            int number = int.Parse(Console.ReadLine());
+            int sumNumber = 0;
+            do
+            {
+                sumNumber += number % 10;
+                number /= 10;
+            }
+            while (number != 0);
+            Console.WriteLine($"S = {sumNumber}");
+        }
+
+    }
+}
