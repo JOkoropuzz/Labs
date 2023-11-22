e521fa5854b5ac3c9b2735a882ea9006e51f6bfd
Nikita <142217578+JOkoropuzz@users.noreply.github.com>
2023-11-22 23:40:46 +0300

    Add lab 04

---
+++ Lab04/STARTER/QuadEquat/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+
+namespace QuadEquat
+{
+    public class Operation
+    {
+        public static int Root(double a, double b, double c, out double x1, out double x2)
+        {
+            int n;
+            double D = (b*b) - (4*a*c);
+            switch (D)
+            {
+                case 0:
+                    x1 = (-b)/(2*a);
+                    x2 = x1;
+                    n = 0;
+                    return n;
+
+                case < 0:
+                    x1 = 0;
+                    x2 = 0;
+                    n = -1;
+                    return n;
+
+                case > 0:
+                    x1 = (Math.Sqrt(D) - b)/ (2 * a);
+                    x2 = (- Math.Sqrt(D) - b) / (2 * a);
+                    n = 1;
+                    return n;
+                default:
+                    x1= 0;
+                    x2= 0;
+                    n = 2;
+                    return n;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first coefficient (a) of quadratic equation: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second coefficient (b) of quadratic equation: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third coefficient (c) of quadratic equation: ");
+            double c = double.Parse(Console.ReadLine());
+            double root1, root2;
+            switch (Operation.Root(a, b, c, out root1, out root2))
+            {
+                case -1:
+                    Console.WriteLine($"Root of the quadratic equation with coefficients a = {a} b = {b} c = {c} NOT exist.");
+                    break;
+                case 0:
+                    Console.WriteLine($"The quadratic equation with coefficients a = {a} b = {b} c = {c} have only one root x1 = x2 = {root1}.");
+                    break;
+                case 1:
+                    Console.WriteLine($"The quadratic equation with coefficients a = {a} b = {b} c = {c} have two roots x1 = {root1} x2 = {root2}.");
+                    break;
+                case 2:
+                default: Console.WriteLine("Something went wrong"); break;
+
+            }
+        }
+    }
+}
---
+++ Lab04/STARTER/TriangSquare/TriangSquare.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace TriangSquare
+{
+
+    public class Operation
+    {
+        public static double TriangSquare(double a, double b, double c) // Расчёт площади треугольника по формуле Герона с проверкой на сущесвтование 
+        {
+            if (TriangExist(a, b, c)) 
+            {
+            double square = Math.Sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
+            return square;
+            }
+            else return 0;
+        }
+
+
+        private static bool TriangExist(double a, double b, double c) // Метод проверки существования треугольника
+        {
+            bool ok = true;
+            if ((a + b) < c) { ok = false; }
+            if ((a + c) < b) { ok = false; }
+            if ((b + c) < a) { ok = false; }
+            return ok;
+        }
+
+
+        public static double TriangSquare(double a) // Расчёт площади равностороннего треугольника
+        {
+            double square = Math.Sqrt((a*3 / 2) * (Math.Pow(((a*3 / 2)-a), 3)));
+            return square;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Is your triangle equilateral? y/n");
+            string answer = Console.ReadLine();
+            if (answer != "y" && answer != "n") { Console.WriteLine("Incorrect input!"); } // Проверка введения y/n
+            else
+            {
+                if (answer == "y") // Равносторонний треугольник
+                {
+                    Console.WriteLine("Enter side of equilateral triangle: ");
+                    double a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Square of triangle = {0}", Operation.TriangSquare(a));
+                }
+                else // Треуголник не равносторонний. Внутри метода Operation.TriangSquare осуществляется провернка
+                {
+                    Console.WriteLine("Enter side a of triangle: ");
+                    double a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter side a of triangle: ");
+                    double b = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter side a of triangle: ");
+                    double c = double.Parse(Console.ReadLine());
+                    if (Operation.TriangSquare(a,b,c) == 0) { Console.WriteLine("Tiangle NOT exist!"); } // Если метод вернул 0, значит треугольника не существует
+                    else Console.WriteLine("Square of triangle = {0}", Operation.TriangSquare(a, b, c));
+                }
+            }
+            
+        }
+    }
+}
