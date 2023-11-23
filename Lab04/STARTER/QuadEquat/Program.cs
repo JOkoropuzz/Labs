2bacfe9c09a45c2ae90db2f572b605a096a14330
Nikita <142217578+JOkoropuzz@users.noreply.github.com>
2023-11-23 21:36:15 +0300

    Add tuple in operation method

--- Lab04/STARTER/QuadEquat/Program.cs
+++ Lab04/STARTER/QuadEquat/Program.cs
@@ -4,34 +4,21 @@ namespace QuadEquat
 {
     public class Operation
     {
-        public static int Root(double a, double b, double c, out double x1, out double x2)
+        public static (double, double, int) Root(double a, double b, double c)
         {
-            int n;
             double D = (b*b) - (4*a*c);
             switch (D)
             {
                 case 0:
-                    x1 = (-b)/(2*a);
-                    x2 = x1;
-                    n = 0;
-                    return n;
+                    return ((-b) / (2 * a), (-b) / (2 * a), 0);
 
-                case < 0:
-                    x1 = 0;
-                    x2 = 0;
-                    n = -1;
-                    return n;
+                case < 0:;
+                    return (0, 0, -1);
 
                 case > 0:
-                    x1 = (Math.Sqrt(D) - b)/ (2 * a);
-                    x2 = (- Math.Sqrt(D) - b) / (2 * a);
-                    n = 1;
-                    return n;
-                default:
-                    x1= 0;
-                    x2= 0;
-                    n = 2;
-                    return n;
+                    return ((Math.Sqrt(D) - b) / (2 * a), (-Math.Sqrt(D) - b) / (2 * a), 1);
+                default: 
+                    return (0, 0, 2);
             }
         }
     }
@@ -45,17 +32,16 @@ namespace QuadEquat
             double b = double.Parse(Console.ReadLine());
             Console.WriteLine("Enter the third coefficient (c) of quadratic equation: ");
             double c = double.Parse(Console.ReadLine());
-            double root1, root2;
-            switch (Operation.Root(a, b, c, out root1, out root2))
+            switch (Operation.Root(a, b, c).Item3)
             {
                 case -1:
                     Console.WriteLine($"Root of the quadratic equation with coefficients a = {a} b = {b} c = {c} NOT exist.");
                     break;
                 case 0:
-                    Console.WriteLine($"The quadratic equation with coefficients a = {a} b = {b} c = {c} have only one root x1 = x2 = {root1}.");
+                    Console.WriteLine($"The quadratic equation with coefficients a = {a} b = {b} c = {c} have only one root x1 = x2 = {Operation.Root(a, b, c).Item1}.");
                     break;
                 case 1:
-                    Console.WriteLine($"The quadratic equation with coefficients a = {a} b = {b} c = {c} have two roots x1 = {root1} x2 = {root2}.");
+                    Console.WriteLine($"The quadratic equation with coefficients a = {a} b = {b} c = {c} have two roots x1 = {Operation.Root(a, b, c).Item1} x2 = {Operation.Root(a, b, c).Item2}.");
                     break;
                 case 2:
                 default: Console.WriteLine("Something went wrong"); break;
