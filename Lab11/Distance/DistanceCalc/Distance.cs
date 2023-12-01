264e772f202b9f964d95d9af10a8a0e60939c6c2
Nikita <142217578+JOkoropuzz@users.noreply.github.com>
2023-12-01 21:16:06 +0300

    Add Alb11

---
+++ Lab11/Distance/DistanceCalc/Distance.cs
@@ -0,0 +1,75 @@
+﻿
+namespace DistanceCalc
+{
+    public class Distance
+    {
+        public int feet;
+        public double inches;
+
+
+        //Конструкторы
+        public Distance()
+        {
+            this.feet = 0;
+            this.inches = 0;
+        }
+
+        public Distance(int feet, double inches)
+        {
+            this.feet = feet;
+            this.inches = inches;
+        }
+
+        //Операторы
+
+        public static Distance operator +(Distance dist1, Distance dist2)
+        {
+            Distance totalDist = new Distance();
+            double sum = (dist1.feet + dist2.feet) * 12 + dist1.inches + dist2.inches;
+            totalDist.inches = sum % 12;
+            totalDist.feet = (int)sum/12;
+            return totalDist;
+        }
+
+        public static Distance operator -(Distance dist1, Distance dist2)
+        {
+            Distance totalDist = new Distance();
+            double sum = (dist1.feet - dist2.feet) * 12 + (dist1.inches - dist2.inches);
+            totalDist.inches = sum % 12;
+            totalDist.feet = (int)sum / 12;
+            return totalDist;
+        }
+
+        public static bool operator >=(Distance dist1, Distance dist2)
+        {
+            double sum = (dist1.feet - dist2.feet) * 12 + (dist1.inches - dist2.inches);
+            if (sum >= 0) return true;
+            else return false;
+        }
+
+        public static bool operator >(Distance dist1, Distance dist2)
+        {
+            double sum = (dist1.feet - dist2.feet) * 12 + (dist1.inches - dist2.inches);
+            if (sum > 0) return true;
+            else return false;
+        }
+
+        public static bool operator <(Distance dist1, Distance dist2)
+        {
+            return !(dist1 >= dist2);
+        }
+
+        public static bool operator <=(Distance dist1, Distance dist2)
+        {
+            return !(dist1 > dist2);
+        }
+
+
+        //Методы
+
+        public override string ToString()
+        {
+            return $"{this.feet}'- {this.inches:F0}\"";
+        }
+    }
+}
---
+++ Lab11/Distance/DistanceCalc/TotalDistance.cs
@@ -0,0 +1,30 @@
+﻿namespace DistanceCalc
+{
+   
+    internal class TotalDistance
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Distance firstDist = new Distance();
+                Distance secondDist = new Distance();
+                Console.WriteLine("Enter feet for the first distance:");
+                firstDist.feet = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter inches for the first distance:");
+                firstDist.inches = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter feet for the second distance:");
+                secondDist.feet = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter inches for the second distance:");
+                secondDist.inches = double.Parse(Console.ReadLine());
+                Distance thirdDist = firstDist + secondDist;
+                Distance fourDist = firstDist - secondDist;
+                Console.WriteLine($"Сумма = {thirdDist.ToString()}\nРазность = {fourDist.ToString()}");
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine($"An exception was throw: {e.Message}");
+            }
+        }
+    }
+}
