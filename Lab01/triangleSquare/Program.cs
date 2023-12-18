2c130063fca5641e675d0890ac430a7298fe89b4
Nikita <142217578+JOkoropuzz@users.noreply.github.com>
2023-12-18 16:35:13 +0300

    Delete Lab01 directory

--- Lab01/triangleSquare/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using System.Reflection.Metadata;
-
-Console.WriteLine("Enter the perimeter of the equilateral triangle");
-double pirimeter = double.Parse(Console.ReadLine());
-double triangleSide = pirimeter / 3;
-double square = Math.Sqrt((pirimeter / 2) * Math.Pow(((pirimeter / 2) - triangleSide), 3));
-Console.WriteLine("Side | Square");
-Console.WriteLine(Math.Round(triangleSide, 2) + " | " + Math.Round(square, 2));
